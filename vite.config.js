6d93b304776a19f8e9e623133e0ac14c957b0123
JLTC3111 <dr.herbal2011@gmail.com>
2025-05-01 09:09:55 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 // vite.config.js
 export default {
-    base: '', // Use '' or './' to keep all paths relative after build
+    base: './', // Use '' or './' to keep all paths relative after build
     server: {
-      historyApiFallback: true,
-    },
+      fs: {
+        strict: false
+      }
+    }
   };
   
