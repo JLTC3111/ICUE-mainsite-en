039059e0be658cf1f0b19d10e3b0a47dd8a529c1
JLTC3111 <dr.herbal2011@gmail.com>
2025-07-03 16:50:00 +0700

    Update script.js

--- src/script.js
+++ src/script.js
@@ -639,7 +639,7 @@ window.initHomeTextSlider = () => {
     `Built on Unity, <strong class="highlight-text-phrase"> Driven by Values! </strong> ​​We believe in <strong class="highlight-text-phrase"> giving back </strong>, and constantly striving for self-improvement. These <strong class="highlight-text-phrase"> core values </strong> ​​shape our approach & inspire our partnerships with local professionals, government agencies. 🤝 `,
     `Smart Cities, Smarter Solutions. We use technology and <strong class="highlight-text-phrase"> data-driven insights </strong> to improve <strong class="highlight-text-phrase"> efficiency </strong>, connectivity, and future-ready cities. 💡 `,
     `Led <strong class="highlight-text-phrase"> Đà Nẵng citywide </strong> planning initiative for both tier 1 and tier 2 cities — a transformational project that reflects our commitment to <strong class="highlight-text-phrase"> big-picture </strong> strategy and real results. 🏆 `,
-    `<strong class="highlight-text-phrase"> Shaping cities </strong>, improving lives . Every solution we deliver is rooted in a mission: to create a better urban future that is inclusive, <strong class="highlight-text-phrase"> sustainable </strong> and <strong class="highlight-text-phrase"> people-centered. </strong> 🌱  `,
+    `Shaping cities, <strong class="highlight-text-phrase"> improving lives. </strong> Every solution we deliver is rooted in a mission: to create a better urban future that is inclusive, <strong class="highlight-text-phrase"> sustainable </strong> and <strong class="highlight-text-phrase"> people-centered. </strong> 🌱  `,
     `💥 Create beautiful <strong class="highlight-text-phrase"> experiences </strong> that last forever.`
   ];
 
