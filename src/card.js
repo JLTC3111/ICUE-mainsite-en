aca7b292318e3a1583b51962c08eccc578f43069
JLTC3111 <dr.herbal2011@gmail.com>
2025-07-30 23:58:54 +0700

    update

--- src/card.js
+++ src/card.js
@@ -4,8 +4,8 @@ const cards = [
     title: "General Planning Adjustment for Lao Cai City",
     description: "Total Area - 28,162.64 hectares, projected completion year is 2045. The plan focuses on optimizing land use, improving infrastructure, guiding population growth, and enhancing the city’s role as a regional economic and transportation hub.",
     images: [
-      "public/pastprojects/project_1/pp_1a.png", 
-      "public/pastprojects/project_1/pp_1b.png",
+      "/pastprojects/project_1/pp_1a.png", 
+      "/pastprojects/project_1/pp_1b.png",
     ],
     year: "2024",
     location: "Lao Cai City"
@@ -15,8 +15,8 @@ const cards = [
     title: "General Construction Planning at 1/500 Scale",
     description: "Total Area:2,693.3 hectares. A detailed 1/500 scale construction plan for Hop Thanh Commune in Lao Cai City, covering 2,693.3 hectares. The plan sets the groundwork for future urban development, zoning, and infrastructure alignment to support sustainable growth by 2025.",
     images: [
-      "public/pastprojects/project_2/pp_2a.png",  
-      "public/pastprojects/project_2/pp_2b.png", 
+      "/pastprojects/project_2/pp_2a.png",  
+      "/pastprojects/project_2/pp_2b.png", 
     ],
     year: "2025",
     location: "Hop Thanh Commune, Lao Cai City"
@@ -32,7 +32,7 @@ cards.forEach(card => {
     .then(template => {
       // Create image HTML from array
       const imagesHTML = card.images
-        .map(src => `<img src="/public${src}" alt="${card.title}" style="width:100%; margin-bottom: 10px;">`)
+        .map(src => `<img src="${src}" alt="${card.title}" style="width:100%; margin-bottom: 10px;">`)
         .join('');
       const cardHTML = template
         .replace(/{{title}}/g, card.title)
