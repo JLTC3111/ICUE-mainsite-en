69db4ba00a20f8a9dae302104ac95e363f37e030
JLTC3111 <dr.herbal2011@gmail.com>
2026-05-18 15:04:08 +0700

    Update script.js
    Signed-off-by: JLTC3111 <dr.herbal2011@gmail.com>

--- src/script.js
+++ src/script.js
@@ -5778,11 +5778,10 @@ function initAudioVisualizer(
 window.preloadProfileImages = () => {
   // Images for meetourexperts.html
   const expertImages = [
-    "public/profilePhotos/hanhnguyen.jpg",
-    "public/profilePhotos/hoangthuha.jpg",
-    "public/profilePhotos/tranthilananh.jpg",
-    "public/profilePhotos/tranquoctoan.jpg",
-    "public/profilePhotos/giaminh.jpg"
+    "public/profilePhotos/hanhnguyen__nobg.png",
+    "public/profilePhotos/tranthilananh__nobg.png",
+    "public/profilePhotos/tranquoctoan__nobg.png",
+    "public/profilePhotos/longdo__nobg.png"
   ];
   // Images for coreTeam.html
   const coreTeamImages = [
