3686e630673dc7c140bd8bce0d68932b75c08930
JLTC3111 <dr.herbal2011@gmail.com>
2026-07-15 11:37:05 +0700

    various updates

--- vite.config.js
+++ vite.config.js
@@ -137,6 +137,7 @@ export default defineConfig({
   plugins: [react(), publicDevFallback(), homeDevFallback()],
   resolve: {
     alias: {
+      '@': path.resolve(__dirname),
       '@icue/main-site-nav': path.resolve(__dirname, 'shared/main-site-nav'),
       '@icue/drawer-menu': path.resolve(__dirname, 'shared/drawer-menu'),
       '@icue/home-layout': path.resolve(__dirname, 'shared/home-layout'),
